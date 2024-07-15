115045a86eeb1ee262289c026aa96976aab30968
HARSHK364 <134734598+HARSHK364@users.noreply.github.com>
2024-07-15 19:07:16 +0530

    collection

---
+++ assignment 4/assignment 4/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, double> products = new Dictionary<string, double>();
+        products.Add("fan", 10.02);
+        products.Add("laptop", 1000.56);
+        products.Add("mouse", 200.6);
+        products.Add("microwave", 2000.56);
+        products.Add("pen", 100);
+
+        Console.WriteLine("\n list of products: ");
+
+        foreach (var prd in products)
+        {
+            Console.WriteLine($"{prd.Key}:{prd.Value}");
+        }
+
+        double averagePrice = products.Values.Average();
+        Console.WriteLine($"Average Price: {averagePrice}");
+
+
+
+        var mostExpensiveProduct = products.OrderByDescending(x => x.Value).First();
+        Console.WriteLine($"Most Expensive Product: {mostExpensiveProduct.Key} - {mostExpensiveProduct.Value}");
+
+    }
+}
