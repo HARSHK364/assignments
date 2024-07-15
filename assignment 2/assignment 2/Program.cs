4d478085644727d9ced44601db319788334c7e01
HARSHK364 <134734598+HARSHK364@users.noreply.github.com>
2024-07-15 19:08:16 +0530

    collection

---
+++ assignment 2/assignment 2/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> Values = new List<int>();
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            Values.Add(i);
+        }
+
+
+        foreach (int Value in Values)
+        {
+            Console.WriteLine(Value);
+        }
+
+        Values.Sort();
+        Values.Reverse();
+        Console.WriteLine("\n after  reverse sorting  ");
+        {
+            foreach (int Value in Values)
+            {
+
+                Console.WriteLine(Value);
+
+            }
+        }
+        Values.Sort();
+        Console.WriteLine("after sorting");
+        {
+            foreach (int Value in Values)
+            {
+                Console.WriteLine(Value);
+            }
+        }
+        int searchNumber = 2;
+        bool found = Values.Contains(searchNumber);
+        if (found)
+        {
+            Console.WriteLine("\n true : " + searchNumber);
+
+        }
+        else
+        {
+            Console.WriteLine("\n false value not found");
+        }
+    }
+}
---
+++ assignment 3/assignment 3/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, int> students = new Dictionary<string, int>();
+        students.Add("harsh", 85);
+        students.Add("Aryan", 82);
+        students.Add("divyansh", 78);
+        students.Add("deepa", 62);
+        students.Add("drishti", 00);
+
+        Console.WriteLine("initially students score are :");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key}:{student.Value}");
+
+        }
+
+        students["drishti"] = 20;
+        Console.WriteLine(" \n updated score");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key}:{student.Value}");
+
+        }
+
+        students.Remove("deepa");
+        Console.WriteLine("\n after removing :");
+        foreach (var student in students)
+        {
+            Console.WriteLine($"{student.Key}:{student.Value}");
+
+        }
+
+        string specificStudent = "harsh";
+
+        if (students.TryGetValue(specificStudent, out int value))
+        {
+            Console.WriteLine($"\nScore of {specificStudent} is:{value}");
+        }
+        else
+        {
+            Console.WriteLine($"Student {specificStudent} not found.");
+        }
+
+        Console.ReadLine();
+    }
+}
