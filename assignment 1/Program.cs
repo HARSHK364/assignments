68d1f9dcaa210a9a3894d3c3db01526fe4628382
HARSHK364 <134734598+HARSHK364@users.noreply.github.com>
2024-07-15 19:10:02 +0530

    collection

---
+++ assignment 1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        List<string> fruits = new List<string>();
+        fruits.Add("Banana");
+        fruits.Add("Apple");
+        fruits.Add("Mango");
+        fruits.Add("Orange");
+        fruits.Add("Grapes");
+
+
+        Console.WriteLine(" list of fruits:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+
+        fruits.Add("Watermelon");
+        Console.WriteLine("\n after adding Watermelon:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+
+        fruits.RemoveAt(1);
+        Console.WriteLine("\n after removing Apple:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        Console.WriteLine("\nThe 4th fruit is: " + fruits[3]);
+
+        bool containsApple = fruits.Contains("Apple");
+        Console.WriteLine("\nList contains Apple: " + containsApple);
+
+ 
+    }
+}
+
+
+
